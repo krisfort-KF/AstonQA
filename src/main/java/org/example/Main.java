a53f0d35fc6a8e05de2016024aee935cdcbd314e
fokina-ka <kristinkad1998@mail.ru>
2025-03-10 20:02:24 +0500

    Домашнее задание к 12 лекции.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,94 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
+        String[][] validArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
 
+        String[][] invalidSizeArray = {
+                {"1", "2", "3"},
+                {"4", "5", "6"}
+        };
 
+        String[][] invalidDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "X", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            System.out.println("Сумма: " + processArray(validArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("Сумма: " + processArray(invalidSizeArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("Сумма: " + processArray(invalidDataArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        
+        throwArrayIndexOutOfBoundsException();
+
+    }
+
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (isInvalidArray(array)) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Ошибка в ячейке: [" + i + "," + j + "]");
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static boolean isInvalidArray(String[][] array) {
+        return array.length != 4
+                || array[0].length != 4
+                || array[1].length != 4
+                || array[2].length != 4
+                || array[3].length != 4;
+    }
+
+    private static void throwArrayIndexOutOfBoundsException() {
+        try {
+            int[] arr = new int[5];
+            System.out.println(arr[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка: выход за границы массива");
+        }
+    }
+}
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
     }
 }
