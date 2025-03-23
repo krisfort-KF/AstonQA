1bd724846d97c9a88c2470189a23a3c9bca82fdf
fokina-ka <kristinkad1998@mail.ru>
2025-03-23 17:08:55 +0500

    Домашнее задание к лекции 15.

--- src/test/java/org/example/ChromeTest.java
+++ src/test/java/org/example/ChromeTest.java
@@ -6,18 +6,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ChromeTest {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeAll
     static void setupAll() {
@@ -27,12 +28,11 @@ class ChromeTest {
     @BeforeEach
     void setup() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
         driver.get("https://www.mts.by/");
+        driver.manage().deleteAllCookies();
 
-        // Закрываем всплывающее окно с куками, если оно появилось
         closeCookiesPopup();
     }
 
@@ -44,29 +44,67 @@ class ChromeTest {
     @Test
     public void testBlockTitle() {
 
-        // Найти заголовок блока "Онлайн пополнение без комиссии"
         String blockTitle = driver.findElement(By.xpath("//div[@class='pay__wrapper']" +
                 "//h2[contains(., 'Онлайн пополнение') and contains(., 'без комиссии')]")).getText();
 
-        // Проверить соответствие текста, убирая возможные переносы строк
         assertEquals("Онлайн пополнение без комиссии", blockTitle.replace("\n", " "), "Название блока не совпадает!");
     }
 
     private void closeCookiesPopup() {
         try {
-            // Найти и нажать кнопку "Принять" в окне с куками
             driver.findElement(By.xpath("//button[@id=\"cookie-agree\"]")).click();
-        } catch (NoSuchElementException e) {
-            // Если окно не появилось, продолжаем тест дальше
+        } catch (NoSuchElementException | ElementNotInteractableException e) {
             System.out.println("Окно с куками не появилось.");
         }
     }
 
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+    @Test
+    void testPaymentLogosPresence() {
+
+        WebElement logoElement1 = driver.findElement(By.xpath("//div[@class='pay__partners']//ul/li/img[contains(@src, 'visa.svg')]"));
+        WebElement logoElement2 = driver.findElement(By.xpath("//div[@class='pay__partners']//ul/li/img[contains(@src, 'visa-verified.svg')]"));
+        WebElement logoElement3 = driver.findElement(By.xpath("//div[@class='pay__partners']//ul/li/img[contains(@src, 'mastercard.svg')]"));
+        WebElement logoElement4 = driver.findElement(By.xpath("//div[@class='pay__partners']//ul/li[img[contains(@src, 'mastercard-secure.svg')]]"));
+        WebElement logoElement5 = driver.findElement(By.xpath("//div[@class='pay__partners']//ul/li[img[contains(@src, 'belkart.svg')]]"));
+
+        assertTrue(logoElement1.isDisplayed(), "Логотип visa не найден на странице!");
+        assertTrue(logoElement2.isDisplayed(), "Логотип mastercard не найден на странице!");
+        assertTrue(logoElement3.isDisplayed(), "Логотип belkart не найден на странице!");
+        assertTrue(logoElement4.isDisplayed(), "Логотип maestro не найден на странице!");
+        assertTrue(logoElement5.isDisplayed(), "Логотип mir не найден на странице!");
     }
 
+    @Test
+    void testServiceDetailsLink() {
+        WebElement detailsLink = driver.findElement(By.xpath("//a[@href='/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/']"));
+
+        assertNotNull(detailsLink, "Ссылка 'Подробнее о сервисе' не найдена");
+        assertTrue(detailsLink.isDisplayed(), "Ссылка 'Подробнее о сервисе' не отображается");
+        assertTrue(detailsLink.isEnabled(), "Ссылка 'Подробнее о сервисе' не активна");
+        detailsLink.click();
+
+        String expectedUrl = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        assertEquals(expectedUrl, driver.getCurrentUrl(), "Некорректный URL после перехода по ссылке");
+    }
+
+    @Test
+    void testOnlinePaymentForm() {
+        WebElement phoneInput = driver.findElement(By.xpath("//form[@id='pay-connection']//input[@id='connection-phone' and @type='text']"));
+        phoneInput.sendKeys("297777777");
+
+        WebElement serviceType = driver.findElement(By.xpath("//div[@class='select__wrapper']/button[@class='select__header']"));
+        if (!serviceType.isSelected()) {
+            serviceType.click();
+        }
+        WebElement amountInput = driver.findElement(By.xpath("//div[@class='input-wrapper input-wrapper_label-right']/input[@id='connection-sum']"));
+        amountInput.sendKeys("5");
+
+        WebElement continueButton = driver.findElement(By.xpath("//button[@type='submit' and text()='Продолжить']"));
+        continueButton.click();
+
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@class='bepaid-iframe']")));
+        WebElement paymentStep = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='app-wrapper__content']")));
+        assertTrue(paymentStep.isDisplayed(), "Форма оплаты не открылась!");
+    }
 }
